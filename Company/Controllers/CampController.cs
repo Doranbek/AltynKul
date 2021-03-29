aa048f7e7d8ad21f950ddae9a9b7c6224d0685b4
Doron Doron <doronkg@yandex.ru>
2021-03-29 23:20:39 +0600

    create campcontroller and views

---
+++ Company/Controllers/CampController.cs
@@ -0,0 +1,128 @@
+﻿using Company.Data;
+using Company.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.Controllers
+{
+    public class CampController : Controller
+    {
+        protected readonly ILogger<HomeController> _logger;
+        protected readonly ApplicationDbContext db;
+
+        public CampController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
+        // GET: CampController
+        public async Task<ActionResult> Index()
+        {
+            var list = await db.Camps.ToListAsync();
+          
+            return View(list);
+        }
+
+        // GET: CampController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CampController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CampVM model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            
+            var Camp = new Camp
+            {
+                
+                Title = model.Title,
+                StartDate = model.StartDate,
+                EndDate = model.EndDate,
+                Specification = model.Specification
+            };
+
+            db.Add(Camp);
+
+            await db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CampController/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var camp = await db.Camps.FindAsync(id);
+            if (camp == null)
+            {
+                return NotFound();
+            }
+            return View(camp);
+        }
+
+        // POST: CampController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Camp camp)
+        {
+            if (id != camp.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                               
+                    db.Update(camp);
+                    await db.SaveChangesAsync();               
+                
+                return RedirectToAction(nameof(Index));
+            }
+            return View(camp);
+        }
+
+        // GET: CampController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var camp = await db.Camps.FirstOrDefaultAsync(m => m.Id == id);
+            if (camp == null)
+            {
+                return NotFound();
+            }
+
+            return View(camp);
+        }
+
+        // POST: CampController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteAsync(int id, Camp camp)
+        {
+            var DelCamp = await db.Camps.FindAsync(id);
+            db.Camps.Remove(camp);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Company/Models/CampVM.cs
@@ -0,0 +1,29 @@
+﻿using Company.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.Models
+{
+       public class CampVM
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Поток")]
+        public string Title { get; set; }
+        //[Display(Name = "Дата начала потока")]
+        //[Required(ErrorMessage = "Дата начала потока не указан")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        //[Display(Name = "Дата конца потока")]
+        //[Required(ErrorMessage = "Дата конца потока не указан")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+        //[Display(Name = "Описание")]
+        public string Specification { get; set; }
+
+    }
+}
